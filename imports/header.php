2fae5d66956572f6a1f18d5f11d3c93dfb8ac573
Uzo Godwin <117761680+DEGREE2466@users.noreply.github.com>
2023-12-19 02:13:05 -0500

    Add files via upload

---
+++ imports/header.php
@@ -0,0 +1,62 @@
+	<nav>
+		<div class="container">
+		<div class="logo">
+			<a href="./">MY<span>STORE</span></a>
+		</div>
+		<div class="navs">
+			<ul>
+				<li><a href="./">Home</a></li>
+				<li class=''> <a href="#">Categories</a> 
+                <ul class="category-dropdown">
+						<li><a href="category.php">Fashion</a></li>
+						<li><a href="category.php">Electronics</a></li>
+						<li><a href="category.php">Phones & Laptops</a></li>
+					</ul>
+				</li> 
+				<li><a href="all-products.php">products</a></li>
+				<li style="padding:15px;"><a href="#">
+					<img src="img/user.png" class="user-icon" style="" />
+				</a>
+					<ul class="account-dropdown">
+						<li><a href="login.php">Login</a></li>
+						<li><a href="sign-up.php">Create account</a></li>
+						<li><a href="manage-account.php">Manage accou19nt</a></li>
+					</ul>
+				</li>
+
+				
+
+			</ul>
+		</div>
+			<button class="mobile-m-call" onclick="mobile_menu();">
+				<img src="img/bar-icon.png" style="width:30px;" />
+			</button>
+		
+	</div>
+	</nav>
+
+<div class="mobile-menu" id="mbmen">
+   <ul class="mobile-mli">
+   	   <li><a href="#">Home</a></li>
+				<li> <a href="#">Categories</a>
+                <ul class="category-dropdown">
+						<li><a href="#">Fashion</a></li>
+						<li><a href="#">Electronics</a></li>
+						<li><a href="#">Phones & Laptops</a></li>
+					</ul>
+				</li>
+				<li><a href="all-products.php">Products</a></li>
+				<li><a href="#">Accounts</a>
+					<ul class="account-dropdown">
+						<li><a href="login.php">Login</a></li>
+						<li><a href="sign-up.php">Create account</a></li>
+						<li><a href="manage-account.php">Manage account</a></li>=0
+ ;					</ul>
+				</li>
+   </ul>
+   <center>
+   	<button style="width:100px;" onclick="closemobile();">Close</button>
+   </center>
+
+   
+</div>
