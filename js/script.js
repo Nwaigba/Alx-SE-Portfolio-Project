51e6a33e83a393b619307fdd7b52c828b9ec2ea4
Uzo Godwin <117761680+DEGREE2466@users.noreply.github.com>
2023-12-19 02:19:24 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function mobile_menu(){
+	let ds = document.getElementById("mbmen");
+	ds.style.display='block';
+}
+
+
+function closemobile(){
+	let ds = document.getElementById("mbmen");
+	ds.style.display='none';
+}
